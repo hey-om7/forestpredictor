a7926aa501f93d16c6c1a71ada9f68717f3848fc
Om Ambarkar <heyom7@gmail.com>
2022-05-20 06:24:26 +0530

    first commit

---
+++ src/screen1_new/Screen1.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./Screen1.scss"
+
+export default function Screen1() {
+    return (
+        <div className='Screen1'>
+            <img src='bg-img.jpg' alt='background image' />
+            <div className="headingTitle">
+                <h1>Forest Fire Predictor</h1>
+                <h4>with 98.25% accuracy</h4>
+            </div>
+        </div>
+    )
+}
